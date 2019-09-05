ef388b09c527209ec16313fa61321d0a586b2fd8
Dan Foad <danielfoad@gmail.com>
2019-09-05 20:06:02 +0100

    Add more mocks

--- foad.dev-backend-app/src/main/scala/com/foad/dev/backend/repository/LabelRepository.scala
+++ foad.dev-backend-app/src/main/scala/com/foad/dev/backend/repository/LabelRepository.scala
@@ -5,6 +5,21 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class LabelRepository {
   def queryAll(): Future[Map[String, String]] = {
-    Future(Map("app.name" -> "foad.dev"))
+    Future(
+      Map(
+        "app.name" -> "foad.dev",
+        "general.name" -> "Dan Foad",
+        "general.role" -> "Associate Software Developer",
+        "general.aboutMe" -> "About Me",
+        "general.featuredProject" -> "Featured Project",
+        "home.header.subline" -> "Currently working with NowTV",
+        "home.header.contact" -> "Contact Me",
+        "app.nav.links.home" -> "Home",
+        "app.nav.links.projects" -> "Projects",
+        "app.nav.links.cv" -> "CV",
+        "app.nav.links.blog" -> "Blog",
+        "app.nav.links.contact" -> "Contact Me"
+      )
+    )
   }
 }
